ae4e76de7b55874724609c4666a610df5d15de26
cyanheart <zhenghanc@gmail.com>
2020-10-31 20:24:09 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import NavBar from "./components/navBar";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 import Home from "./components/home";
-import Products from "./components/products";
 import NotFound from "./components/notFound";
-import ContactUs from "./components/contactUs";
 
 function App() {
-  return (
-    <React.Fragment>
-      {/* <NavBar></NavBar> */}
-      <Switch>
-        <Route path="/products" component={Products}></Route>
-        <Route path="/contact-us" component={ContactUs}></Route>
-        <Route path="/home" component={Home}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" to="/home" exact></Redirect>
-        <Redirect to="/not-found"></Redirect>
-      </Switch>
-    </React.Fragment>
-  );
+  return <Home/>
+  // return (
+  //   <React.Fragment>
+  //     <HashRouter basename="/">
+  //       <Switch>
+  //         <Route path="/home" component={Home}></Route>
+  //         <Route path="/not-found" component={NotFound}></Route>
+  //         <Redirect from="/" to="/home" exact></Redirect>
+  //         <Redirect to="/not-found"></Redirect>
+  //       </Switch>
+  //     </HashRouter>
+  //   </React.Fragment>
+  // );
 }
 
 export default App;
